9dc085f34092d195b301ab4260a542fa8dbb7763
Carlos Krefer <carloskrefer@hotmail.com>
2024-12-25 18:19:22 -0300

    adiciona componente que lista projetos do usuário
    -componente servirá pra alternar qual projeto atual

---
+++ src/app/features/navigation/navigation-project-sheet/navigation-project-sheet.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'app-navigation-project-sheet',
+  imports: [MatListModule],
+  standalone: true,
+  templateUrl: './navigation-project-sheet.component.html',
+  styleUrl: './navigation-project-sheet.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NavigationProjectSheetComponent {
+  private bottomSheetRef =
+    inject<MatBottomSheetRef<NavigationProjectSheetComponent>>(MatBottomSheetRef);
+
+}
