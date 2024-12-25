ea341a93d54427684f06b75247fcc02a40fa399e
Carlos Krefer <carloskrefer@hotmail.com>
2024-12-25 18:20:50 -0300

    adiciona componente principal, com sidenav e main content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './features/navigation/navigation.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [
-      RouterOutlet,
-      MatSlideToggleModule
-    ],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    MatSlideToggleModule,
+    NavigationComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
 
