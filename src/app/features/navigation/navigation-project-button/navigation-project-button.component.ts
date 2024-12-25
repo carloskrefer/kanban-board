3edf09a494c87fa4c300a1921e9aa9860fe1987b
Carlos Krefer <carloskrefer@hotmail.com>
2024-12-25 18:19:39 -0300

    adiciona componente botão para acesso a lista de projetos

---
+++ src/app/features/navigation/navigation-project-button/navigation-project-button.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { NavigationProjectSheetComponent } from '../navigation-project-sheet/navigation-project-sheet.component';
+
+@Component({
+  selector: 'app-navigation-project-button',
+  imports: [MatButtonModule, MatIconModule],
+  standalone: true,
+  templateUrl: './navigation-project-button.component.html',
+  styleUrl: './navigation-project-button.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavigationProjectButtonComponent {
+  bottomSheet = inject(MatBottomSheet);
+
+  openBottomSheet() {
+    this.bottomSheet.open(NavigationProjectSheetComponent);
+  }
+}
